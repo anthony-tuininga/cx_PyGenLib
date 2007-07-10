cadb81e3661c2a7260600b98e0df9a2d88573bca
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2007-07-10 22:14:12 +0000

    First cut at data sets which allow for keeping track of inserts, updates and deletes and performs all of these in one transaction when desired; particularly useful with the ceGUI.Grid class.

--- ceDatabase.py
+++ ceDatabase.py
@@ -2,6 +2,8 @@
 Define classes and methods suitable for accessing databases in a generic way.
 """
 
+import cx_Logging
+
 def _NormalizeValue(bases, classDict, name):
     """Helper routine for row metaclass."""
     value = classDict.get(name)
@@ -24,6 +26,7 @@ class RowMetaClass(type):
     def __new__(cls, name, bases, classDict):
         attrNames = _NormalizeValue(bases, classDict, "attrNames")
         extraAttrNames = _NormalizeValue(bases, classDict, "extraAttrNames")
+        _NormalizeValue(bases, classDict, "pkAttrNames")
         useSlots = _NormalizeValue(bases, classDict, "useSlots")
         if useSlots:
             classDict["__slots__"] = attrNames + extraAttrNames
@@ -41,15 +44,17 @@ class RowMetaClass(type):
 
 
 class Row(object):
+    """Base class for use with the row meta class (see above)."""
     __metaclass__ = RowMetaClass
     __slots__ = []
     attrNames = []
     extraAttrNames = []
+    pkAttrNames = []
     useSlots = True
 
     def __repr__(self):
-        if self.__slots__:
-            values = ["%s=%r" % (n, getattr(self, n)) for n in self.__slots__]
+        if self.attrNames:
+            values = ["%s=%r" % (n, getattr(self, n)) for n in self.attrNames]
             return "<%s %s>" % (self.__class__.__name__, ", ".join(values))
         return "<%s>" % self.__class__.__name__
 
@@ -57,4 +62,166 @@ class Row(object):
         cls = self.__class__
         args = [getattr(self, n) for n in cls.attrNames]
         return cls(*args)
+
+
+class DataSetMetaClass(type):
+    """Metaclass for data sets which sets up the class used for retrieval and
+       other data manipulation routines."""
+
+    def __init__(cls, name, bases, classDict):
+        super(DataSetMetaClass, cls).__init__(name, bases, classDict)
+        cls.rowClass = RowMetaClass("%sRow" % name, (Row,),
+                dict(attrNames = cls.attrNames))
+        cls.attrNames = cls.rowClass.attrNames
+        if isinstance(cls.pkAttrNames, basestring):
+            cls.pkAttrNames = cls.pkAttrNames.split()
+        if isinstance(cls.retrievalAttrNames, basestring):
+            cls.retrievalAttrNames = cls.retrievalAttrNames.split()
+
+
+class DataSet(object):
+    """Base class for data sets which allows for retrieval, insert, update and
+       deletion of rows in a database."""
+    __metaclass__ = DataSetMetaClass
+    tableName = None
+    attrNames = []
+    pkAttrNames = []
+    retrievalAttrNames = []
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.Clear()
+
+    def _DeleteRowsInDatabase(self, cursor):
+        cx_Logging.Debug("deleting rows in database....")
+        for row in self.deletedRows.itervalues():
+            self.DeleteRowInDatabase(cursor, row)
+
+    def _GetNewRowHandle(self):
+        if self.rows:
+            handle = max(self.rows) + 1
+        else:
+            handle = 0
+        if self.deletedRows:
+            existingHandle = max(self.deletedRows)
+            if existingHandle >= handle:
+                handle = existingHandle + 1
+        return handle
+
+    def _InsertRowsInDatabase(self, cursor):
+        for row in self.insertedRows.itervalues():
+            self.InsertRowInDatabase(cursor, row)
+
+    def _OnDeleteRow(self, row):
+        pass
+
+    def _OnInsertRow(self, row):
+        pass
+
+    def _OnSetValue(self, row, attrName, value, origValue):
+        pass
+
+    def _PostUpdate(self):
+        pass
+
+    def _PreUpdate(self):
+        pass
+
+    def _UpdateRowsInDatabase(self, cursor):
+        for handle, origRow in self.updatedRows.iteritems():
+            row = self.rows[handle]
+            self.UpdateRowInDatabase(cursor, row, origRow)
+
+    def CanDeleteRow(self, rowHandle):
+        return True
+
+    def CanInsertRow(self):
+        return True
+
+    def Clear(self):
+        self.rows = {}
+        self.insertedRows = {}
+        self.updatedRows = {}
+        self.deletedRows = {}
+
+    def DeleteRow(self, handle):
+        row = self.rows[handle]
+        self._OnDeleteRow(row)
+        self.rows.pop(handle)
+        self.deletedRows[handle] = row
+
+    def DeleteRowInDatabase(self, cursor, row):
+        args = [getattr(row, n) for n in self.pkAttrNames]
+        clauses = ["%s = ?" % n for n in self.pkAttrNames]
+        sql = "delete from %s where %s" % \
+                (self.tableName, " and ".join(clauses))
+        cursor.execute(sql, args)
+
+    def InsertRow(self):
+        handle = self._GetNewRowHandle()
+        row = self.rowClass.New()
+        self._OnInsertRow(row)
+        self.insertedRows[handle] = self.rows[handle] = row
+        return handle, row
+
+    def InsertRowInDatabase(self, cursor, row):
+        args = [getattr(row, n) for n in self.attrNames]
+        names = [n for n in self.attrNames]
+        values = ["?"] * len(self.attrNames)
+        sql = "insert into %s (%s) values (%s)" % \
+                (self.tableName, ",".join(names), ",".join(values))
+        cursor.execute(sql, args)
+
+    def Retrieve(self, *args):
+        self.Clear()
+        sql = "select %s from %s" % (", ".join(self.attrNames), self.tableName)
+        if self.retrievalAttrNames:
+            whereClauses = ["%s = ?" % n for n in self.retrievalAttrNames]
+            sql += " where %s" % " and ".join(whereClauses)
+        cursor = self.connection.cursor()
+        cursor.execute(sql, args)
+        cursor.rowfactory = self.rowClass
+        self.rows = dict(enumerate(cursor))
+
+    def SetValue(self, handle, attrName, value):
+        row = self.rows[handle]
+        origValue = getattr(row, attrName)
+        if value != origValue:
+            if handle not in self.insertedRows \
+                    and handle not in self.updatedRows:
+                self.updatedRows[handle] = row.Copy()
+            cx_Logging.Debug("setting attr %s on row %s to %s", attrName,
+                    handle, value)
+            self._OnSetValue(row, attrName, value, origValue)
+            setattr(row, attrName, value)
+
+    def Update(self):
+        if not self.insertedRows and not self.updatedRows \
+                and not self.deletedRows:
+            cx_Logging.Debug("no update to perform")
+            return
+        self._PreUpdate()
+        cursor = self.connection.cursor()
+        try:
+            if self.deletedRows:
+                self._DeleteRowsInDatabase(cursor)
+            if self.updatedRows:
+                self._UpdateRowsInDatabase(cursor)
+            if self.insertedRows:
+                self._InsertRowsInDatabase(cursor)
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+            raise
+        self._PostUpdate()
+
+    def UpdateRowInDatabase(self, cursor, row, origRow):
+        dataAttrNames = [n for n in self.attrNames if n not in self.pkAttrNames]
+        args = [getattr(row, n) for n in dataAttrNames + self.pkAttrNames]
+        setClauses = ["%s = ?" % n for n in dataAttrNames]
+        whereClauses = ["%s = ?" % n for n in self.pkAttrNames]
+        sql = "update %s set %s where %s" % \
+                (self.tableName, ", ".join(setClauses),
+                " and ".join(whereClauses))
+        cursor.execute(sql, args)
 
